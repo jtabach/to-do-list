70d2ca37612228a953d4f4348702a91556526a5e
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-12 16:50:00 -0800

    functional to do list

--- main.js
+++ main.js
@@ -4,18 +4,32 @@ $(document).ready(init);
 var classAdds = ['tasks', 'dates', 'done', 'remove'];
 var descr = [];
 
+console.log(moment("20160131", "YYYYMMDD").fromNow());
 
 function init() {
 	$('#add').on('click', addItem);
-	$('.remove').on('click', removeTask);
 }
 
 function addItem() {
 	descr[0] = $('#task').val();
-	descr[1] = $('#due').val();
+	manipulateDate();
 	descr[2] = $('<div class="checkbox"><label><input type="checkbox"> Done</label></div>');
 	descr[3] = $('<button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>');
-	appendTask();
+	evalEntry();
+}
+
+function evalEntry() {
+	if ($('#task').val() !== "") {
+		appendTask();
+	} else {
+		alert("Enter a task");
+	}
+}
+
+function manipulateDate() {
+	var $due = $('#due');
+	var dateString = $due.val().replace(/-/g,"");
+	descr[1] = moment(dateString, "YYYYMMDD").fromNow();
 }
 
 function appendTask() {
@@ -25,9 +39,19 @@ function appendTask() {
 		var $td = $('<td>').addClass(val).append(descr[i]);
 		$('tbody > tr:last-child').append($td);
 	});
+	$('.remove > button').on('click', removeTask); // Duplicate invocations
+	$('label :checkbox').on('click', toggleCompleteTask); // Duplicate invocations
 }
 
 function removeTask() {
-	$this = $(this);
- 
+ 	$(this).parent().parent().remove(); 
+}
+
+function toggleCompleteTask() {
+	var $this = $(this);
+	if ($this.is(':checked')) {
+		$this.closest('.done').parent().addClass('success');
+	} else {
+		$this.closest('.done').parent().removeClass('success');
+	}
 }
